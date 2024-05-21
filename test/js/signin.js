181744c1b4980ef8a8aa1607f398a2eebcf55071
hyein <yoohi960201@gmail.com>
2024-05-21 16:18:57 +0900

    refactor: 비밀번호 입력 input type 변경

--- test/js/signin.js
+++ test/js/signin.js
@@ -1,5 +1,23 @@
 // signin 페이지 js
 
+// 비밀번호 확인(input id: userPassCheck)과 비밀번호(input id: userPass)를 비교하는 함수
+function validatePassword() {
+    var password = document.getElementById("userPass").value;
+    var confirmPassword = document.getElementById("userPassCheck").value;
+
+    // 비밀번호와 비밀번호 확인이 같은지 확인
+    if (password != confirmPassword) {
+        // 불일치할 경우 경고 메시지를 표시
+        document.getElementById("passError").innerHTML = "비밀번호가 일치하지 않습니다.";
+    } else {
+        // 일치할 경우 경고 메시지를 지우고, 유효성 검사 통과
+        document.getElementById("passError").innerHTML = "";
+    }
+}
+
+// 비밀번호 확인(input id: userPassCheck)이 변경될 때마다 validatePassword 함수 호출
+document.getElementById("userPassCheck").addEventListener("keyup", validatePassword);
+
 document.querySelector('.domainBt').addEventListener('click', function () {
     const domainList = document.querySelector('.domainList');
     domainList.classList.toggle('active');
@@ -14,6 +32,7 @@ document.querySelectorAll('.domainOption').forEach(function (domainButton) {
         document.querySelector('.domainList').classList.remove('active');
     });
 });
+
 const yearSelect = document.getElementById('birthYear');
 const currentYear = new Date().getFullYear();
 for (let year = currentYear; year >= 1900; year--) {
