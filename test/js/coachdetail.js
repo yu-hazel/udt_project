f16654ef9635b34627d30a8b4e080680f64555a0
이승빈 <tmdfufqls1234@gmail.com>
2024-06-13 12:45:17 +0900

    이승빈 기능

--- test/js/coachdetail.js
+++ test/js/coachdetail.js
@@ -1,10 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // 모든 fa-heart 클래스를 가진 요소들을 선택
-  const heartIcons = document.querySelectorAll(".fa-heart");
-  heartIcons.forEach(function (icon) {
-    icon.addEventListener("click", function () {
-      // 클릭할 때마다 'red' 클래스를 토글
-      icon.classList.toggle("red");
+// 하트 버튼을 눌렀을 때 하트가 빨간색으로 차오르도록 딸깍이 구현
+const hearts = document.querySelectorAll("i.fa-heart");
+    hearts.forEach((each) => {
+      each.addEventListener("click", function () {
+        this.classList.toggle("red");
+      });
     });
+
+    
+// coachinfo의 스크롤과 웹 브라우저의 스크롤을 탑버튼을 누르게 되었을 시 위로 이동하도록 구현
+document.querySelector('.scroll').addEventListener('click', function() {
+  const coachInfo = document.querySelector('.coachInfo');
+  coachInfo.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
 });
